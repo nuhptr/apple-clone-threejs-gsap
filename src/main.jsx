35bf3708ff28043d7a5f0c28b58b1bb98665cdba
nuhptr <nugrahaadi733@gmail.com>
2024-04-11 22:19:20 +0700

    main: update code

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 
-import App from "./app.jsx"
 import "./index.css"
+import App from "./app.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
