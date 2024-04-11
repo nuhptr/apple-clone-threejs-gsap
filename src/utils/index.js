cb4bb15e978645e9109200b985911dfeaadaeae4
nuhptr <nugrahaadi733@gmail.com>
2024-04-11 22:16:03 +0700

    update constant and utils

---
+++ src/utils/index.js
@@ -0,0 +1,57 @@
+import hero from "/assets/images/img_hero.png"
+
+export const heroImg = hero
+
+import hmv from "/assets/videos/vid_hero.mp4"
+import smallmv from "/assets/videos/vid_smallHero.mp4"
+import highlightFirstmv from "/assets/videos/vid_highlight-first.mp4"
+import highlightSectmv from "/assets/videos/vid_highlight-sec.mp4"
+import highlightThirdmv from "/assets/videos/vid_highlight-third.mp4"
+import highlightFourthmv from "/assets/videos/vid_highlight-fourth.mp4"
+import exploremv from "/assets/videos/vid_explore.mp4"
+import framemv from "/assets/videos/frame.mp4"
+
+export const heroVideo = hmv
+export const smallHeroVideo = smallmv
+export const highlightFirstVideo = highlightFirstmv
+export const highlightSecondVideo = highlightSectmv
+export const highlightThirdVideo = highlightThirdmv
+export const highlightFourthVideo = highlightFourthmv
+export const exploreVideo = exploremv
+export const frameVideo = framemv
+
+import apple from "/assets/images/ic_apple.svg"
+import search from "/assets/images/ic_search.svg"
+import bag from "/assets/images/ic_bag.svg"
+import watch from "/assets/images/ic_watch.svg"
+import right from "/assets/images/ic_right.svg"
+import replay from "/assets/images/ic_replay.svg"
+import play from "/assets/images/ic_play.svg"
+import pause from "/assets/images/ic_pause.svg"
+
+export const appleImg = apple
+export const searchImg = search
+export const bagImg = bag
+export const watchImg = watch
+export const rightImg = right
+export const replayImg = replay
+export const playImg = play
+export const pauseImg = pause
+
+import yellow from "/assets/images/img_yellow.png"
+import blue from "/assets/images/img_blue.png"
+import white from "/assets/images/img_white.png"
+import black from "/assets/images/img_black.png"
+import explore1 from "/assets/images/img_explore1.png"
+import explore2 from "/assets/images/img_explore2.png"
+import chip from "/assets/images/img_chip.png"
+import frame from "/assets/images/img_frame.png"
+
+export const yellowImg = yellow
+export const blueImg = blue
+export const whiteImg = white
+export const blackImg = black
+export const explore1Img = explore1
+export const explore2Img = explore2
+export const chipImg = chip
+export const frameImg = frame
