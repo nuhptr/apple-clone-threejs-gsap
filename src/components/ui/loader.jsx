003a5ba1dd94e4e7b6c95ec5f593453f450a9470
nuhptr <nugrahaadi733@gmail.com>
2024-04-15 21:53:09 +0700

    ui: loader formatting code

--- src/components/ui/loader.jsx
+++ src/components/ui/loader.jsx
@@ -1,11 +1,11 @@
 import { Html } from "@react-three/drei"
 
 export default function Loader() {
-    return (
-        <Html>
-            <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-                <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
-            </div>
-        </Html>
-    )
+   return (
+      <Html>
+         <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+            <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+         </div>
+      </Html>
+   )
 }
