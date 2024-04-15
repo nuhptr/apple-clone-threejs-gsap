e0c74118aa37918c0af2c868d2ba5de3390ec3c8
nuhptr <nugrahaadi733@gmail.com>
2024-04-15 21:50:06 +0700

    section: highlights update code

--- src/components/section/highlights.jsx
+++ src/components/section/highlights.jsx
@@ -5,35 +5,35 @@ import { rightImg, watchImg } from "../../utils"
 import { VideoCarousel } from "../index"
 
 const Highlights = () => {
-    useGSAP(() => {
-        gsap.to("#title", { opacity: 1, y: 0 })
-        gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
-    }, [])
+   useGSAP(() => {
+      gsap.to("#title", { opacity: 1, y: 0 })
+      gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+   }, [])
 
-    return (
-        <section id="highlights" className="w-screen h-full overflow-hidden common-padding bg-zinc">
-            <div className="screen-max-width">
-                <div className="items-end justify-between w-full mb-12 md:flex">
-                    <h1 id="title" className="section-heading">
-                        Get the highlights.
-                    </h1>
+   return (
+      <section id="highlights" className="w-screen h-full overflow-hidden common-padding bg-zinc">
+         <div className="screen-max-width">
+            <div className="items-end justify-between w-full mb-12 md:flex">
+               <h1 id="title" className="section-heading">
+                  Get the highlights.
+               </h1>
 
-                    <div className="flex flex-wrap items-end gap-5">
-                        <p className="link">
-                            Watch the film{""}
-                            <img src={watchImg} alt="Watch" className="ml-2" />
-                        </p>
-                        <p className="link">
-                            Watch the event{""}
-                            <img src={rightImg} alt="Right" className="ml-2" />
-                        </p>
-                    </div>
-                </div>
-
-                <VideoCarousel />
+               <div className="flex flex-wrap items-end gap-5">
+                  <p className="link">
+                     Watch the film{""}
+                     <img src={watchImg} alt="Watch" className="ml-2" />
+                  </p>
+                  <p className="link">
+                     Watch the event{""}
+                     <img src={rightImg} alt="Right" className="ml-2" />
+                  </p>
+               </div>
             </div>
-        </section>
-    )
+
+            <VideoCarousel />
+         </div>
+      </section>
+   )
 }
 
 export default Highlights
