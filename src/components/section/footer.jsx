f91abd1318cedfaf76d2b5b7fe686edefe39c22e
nuhptr <nugrahaadi733@gmail.com>
2024-04-15 21:54:58 +0700

    section: footer update code

--- src/components/section/footer.jsx
+++ src/components/section/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div>
                <p className="text-xs font-semibold text-gray">
                   More ways to shop:{" "}
-                  <span className="underline text-blue">Find an Apple Store: </span>
+                  <span className="underline text-blue">Find an Apple Store </span>
                   or <span className="underline text-blue">other retailer</span> near you.
                </p>
                <p className="text-xs font-semibold text-gray">Or call 000800-040-1966</p>
@@ -17,7 +17,7 @@ const Footer = () => {
 
             <div className="flex flex-col justify-between md:flex-row md:items-center">
                <p className="text-xs font-semibold text-gray">
-                  Copright @ 2024 Apple Inc. All rights reserved.
+                  Copyright @ 2024 Apple Inc. All rights reserved.
                </p>
                <div className="flex">
                   {footerLinks.map((link, i) => (
