faccec71fd16cc63d6d3fbc5ec402113d7dd1e7b
nuhptr <nugrahaadi733@gmail.com>
2024-04-15 21:52:40 +0700

    three: model-view update code

--- src/components/three/model-view.jsx
+++ src/components/three/model-view.jsx
@@ -6,46 +6,42 @@ import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import { Lights, IPhone, Loader } from "../index"
 
 export default function ModelView({
-    index,
-    groupRef,
-    gsapType,
-    controlRef,
-    setRotationState,
-    item,
-    size,
+   index,
+   groupRef,
+   gsapType,
+   controlRef,
+   setRotationState,
+   item,
+   size,
 }) {
-    return (
-        <View
-            index={index}
-            id={gsapType}
-            className={`absolute w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}>
-            {/* Ambient light */}
-            <ambientLight intensity={10} />
+   return (
+      <View
+         index={index}
+         id={gsapType}
+         className={`absolute w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}>
+         {/* Ambient light */}
+         <ambientLight intensity={10} />
 
-            <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+         <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-            <Lights />
+         <Lights />
 
-            <OrbitControls
-                makeDefault
-                ref={controlRef}
-                enableZoom={false}
-                enablePan={false}
-                rotateSpeed={0.4}
-                target={new THREE.Vector3(0, 0, 0)}
-                // get the angle
-                onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-            />
+         <OrbitControls
+            makeDefault
+            ref={controlRef}
+            enableZoom={false}
+            enablePan={false}
+            rotateSpeed={0.4}
+            target={new THREE.Vector3(0, 0, 0)}
+            // get the angle
+            onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+         />
 
-            <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
-                <Suspense fallback={<Loader />}>
-                    <IPhone
-                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-                        item={item}
-                        size={size}
-                    />
-                </Suspense>
-            </group>
-        </View>
-    )
+         <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
+            <Suspense fallback={<Loader />}>
+               <IPhone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
+            </Suspense>
+         </group>
+      </View>
+   )
 }
